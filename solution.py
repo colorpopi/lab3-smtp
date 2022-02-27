e188624f102259de00abbf02febea4cce90f72ac
Diane <colorpop@Dianes-MBP.fios-router.home>
2022-02-26 19:57:26 -0500

    edits

--- solution.py
+++ solution.py
@@ -1,13 +1,11 @@
 from socket import *
 
 
-def smtp_client(port=1025, mailserver='127.0.0.1'):
+def smtp_client(port=1025, mailserver="127.0.0.1"):
     msg = "\r\n My message"
     endmsg = "\r\n.\r\n"
 
     # Choose a mail server (e.g. Google mail server) if you want to verify the script beyond GradeScope
-    mail_server = "127.0.0.1:1025"
-    # port_num = 1025
 
     # Create socket called clientSocket and establish a TCP connection with mailserver and port
     clientSocket = socket(AF_INET, SOCK_STREAM)
@@ -16,15 +14,15 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     # Fill in end
 
     recv = clientSocket.recv(1024).decode()
-    #print(recv) #You can use these print statement to validate return codes from the server.
-    #if recv[:3] != '220':
+    # print(recv) #You can use these print statement to validate return codes from the server.
+    # if recv[:3] != '220':
     #    print('220 reply not received from server.')
 
     # Send HELO command and print server response.
     heloCommand = 'HELO Alice\r\n'
     clientSocket.send(heloCommand.encode())
     recv1 = clientSocket.recv(1024).decode()
-    #print(recv1) 
+    # print(recv1)
 
     # Send MAIL FROM command and handle server response.
     # Fill in start
@@ -61,7 +59,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
     # Send QUIT command and handle server response.
     clientSocket.send("QUIT \r\n".encode())
-    quit_msg = clientSocket.recv(1024.decode())
+    quit_msg = clientSocket.recv(1024).decode()
     clientSocket.close()
     # Fill in start
     # Fill in end
